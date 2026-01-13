3bcda4725ce94f72e9483355a0e1acde980dd2b4
VJam <vjam@VJams-MacBook-Pro.local>
2026-01-14 03:35:14 +0530

    added games section partially

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   const games = [
@@ -17,10 +16,12 @@ export default function Home() {
     <div className="mt-[120px] md:mt-[200px] px-6">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {games.map((game) => (
-          <Link
+          <a
             key={game.id}
-            href={`https://gdg-website-kappa.vercel.app/gamess/${game.id}`}
+            href={`https://gdg-website-kappa.vercel.app/games/${game.id}`}
             className={`rounded-xl overflow-hidden ${game.bg} group shadow-lg`}
+            target="_blank" // optional: opens in a new tab
+            rel="noopener noreferrer"
           >
             {/* Image */}
             <div className="relative aspect-video">
@@ -36,7 +37,7 @@ export default function Home() {
             <div className="p-4 bg-black/60 text-white">
               <h3 className="text-lg font-bold">{game.title}</h3>
             </div>
-          </Link>
+          </a>
         ))}
       </div>
     </div>
