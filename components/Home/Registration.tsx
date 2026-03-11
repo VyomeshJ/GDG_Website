cd98fa13af604b6dd6676ac1bca177e3b181df5f
VJam <vjam@wifi-172-23-98-141.uoa-wifi.auckland.ac.nz>
2026-03-12 00:43:51 +1300

    fixed scrolling again

--- components/Home/Registration.tsx
+++ components/Home/Registration.tsx
@@ -16,12 +16,12 @@ export default function Registration({ overlay, showOverlay }: RegistrationProps
         w-full max-w-2xl max-h-[85vh] flex flex-col pointer-events-auto"
       >
 
-        {/* Cards (scrollable on mobile) */}
+        {/* Cards */}
         <div className="flex flex-col md:flex-row gap-6 md:gap-10 mb-8 overflow-y-auto flex-1 pr-2">
 
           {/* New Member */}
           <div className="relative bg-[#5B7546]/70 hover:bg-[#5B7546]/70
-              w-full md:w-1/2 aspect-[3/4] rounded-xl overflow-hidden group">
+              w-full md:w-1/2 aspect-[3/4] rounded-xl group">
 
             <Link
               href="https://docs.google.com/forms/d/e/1FAIpQLSdPH6E81By17CAnS-qLgGFvA3zdNR7k9PieZzknwqFOjG6CZQ/viewform?usp=dialog"
@@ -35,7 +35,7 @@ export default function Registration({ overlay, showOverlay }: RegistrationProps
               </h2>
             </div>
 
-            <div className="absolute bottom-0 w-full h-2/3 bg-[#5B7546]/100 md:bg-[#5B7546]/0 group-hover:bg-[#5B7546]/100 rounded-t-xl shadow-[0_-6px_20px_rgba(0,0,0,0)] group-hover:shadow-[0_-6px_20px_rgba(0_-6px_20px_rgba(0,0,0,0.3))]">
+            <div className="absolute bottom-0 w-full h-2/3 bg-[#5B7546]/100 md:bg-[#5B7546]/0 group-hover:bg-[#5B7546]/100 rounded-xl shadow-[0_-6px_20px_rgba(0,0,0,0)] group-hover:shadow-[0_-6px_20px_rgba(0,0,0,0.3)]">
               <div className="relative w-full h-full p-6 md:p-12">
                 <Image
                   src="/icons/g_dude.png"
@@ -49,7 +49,7 @@ export default function Registration({ overlay, showOverlay }: RegistrationProps
 
           {/* Returning Member */}
           <div className="relative bg-[#8A424F]/70 
-              w-full md:w-1/2 aspect-[3/4] rounded-xl overflow-hidden group">
+              w-full md:w-1/2 aspect-[3/4] rounded-xl group">
 
             <Link
               href="https://docs.google.com/forms/d/e/1FAIpQLSe45-GrFQn55PWSOduzYrMiJ3_vpm_sHCfFuNLeVSm6ueNNEA/viewform?usp=dialog"
@@ -57,7 +57,7 @@ export default function Registration({ overlay, showOverlay }: RegistrationProps
               className="absolute inset-0 z-10"
             />
 
-            <div className="absolute top-0 w-full h-2/3 bg-[#8A424F]/100 md:bg-[#8A424F]/0 group-hover:bg-[#8A424F]/100 rounded-b-xl shadow-[0_-6px_20px_rgba(0,0,0,0)] group-hover:shadow-[0_-6px_20px_rgba(0,0,0,0.3)]">
+            <div className="absolute top-0 w-full h-2/3 bg-[#8A424F]/100 md:bg-[#8A424F]/0 group-hover:bg-[#8A424F]/100 rounded-xl shadow-[0_-6px_20px_rgba(0,0,0,0)] group-hover:shadow-[0_-6px_20px_rgba(0,0,0,0.3)]">
               <div className="relative w-full h-full p-6 md:p-12">
                 <Image
                   src="/icons/r_dude.png"
@@ -77,7 +77,7 @@ export default function Registration({ overlay, showOverlay }: RegistrationProps
 
           {/* Executive Member */}
           <div className="relative bg-[#3d4770]/70 hover:bg-[#3d4770]/70
-              w-full md:w-1/2 aspect-[3/4] rounded-xl overflow-hidden group">
+              w-full md:w-1/2 aspect-[3/4] rounded-xl group">
 
             <Link
               href="https://docs.google.com/forms/d/e/1FAIpQLSdGx8fPm7C6vBPbaHIpBz2xIGJnc9PzTFp8lfXRu5CWEx4PxA/viewform?usp=dialog"
@@ -91,7 +91,7 @@ export default function Registration({ overlay, showOverlay }: RegistrationProps
               </h2>
             </div>
 
-            <div className="absolute bottom-0 w-full h-2/3 bg-[#48558a]/100 md:bg-[#48558a]/0 group-hover:bg-[#48558a]/100 rounded-t-xl shadow-[0_-6px_20px_rgba(0,0,0,0)] group-hover:shadow-[0_-6px_20px_rgba(0,0,0,0.3)]">
+            <div className="absolute bottom-0 w-full h-2/3 bg-[#48558a]/100 md:bg-[#48558a]/0 group-hover:bg-[#48558a]/100 rounded-xl shadow-[0_-6px_20px_rgba(0,0,0,0)] group-hover:shadow-[0_-6px_20px_rgba(0,0,0,0.3)]">
               <div className="relative w-full h-full p-6 md:p-12">
                 <Image
                   src="/icons/b_dude.png"
@@ -105,7 +105,7 @@ export default function Registration({ overlay, showOverlay }: RegistrationProps
 
         </div>
 
-        {/* Footer Text */}
+        {/* Footer */}
         <h2 className="font-SL text-xl md:text-3xl text-center">
           Anyone can join, even if you’re a grad or non-UOA!  
           Membership is just $5 per sem~
